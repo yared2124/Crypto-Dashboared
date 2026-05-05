f328e4b3c56c7f9fc33d44b53688a55d11930ff7
yared aregayehu <yaredbaby21@gmail.com>
2026-05-05 16:02:05 -0500

    `Added User model with database interactions`

---
+++ backend/config/constants.js
@@ -0,0 +1,18 @@
+export const HTTP_STATUS = {
+  OK: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+  SERVER_ERROR: 500,
+};
+
+export const USER_ROLES = {
+  USER: "user",
+  ADMIN: "admin",
+};
+
+export const ALERT_DIRECTIONS = ["above", "below"];
+export const TRANSACTION_TYPES = ["buy", "sell"];
