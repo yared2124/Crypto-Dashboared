679fbc319788a0c4f4e7636cb8604bc7142cc9b5
yared aregayehu <yaredbaby21@gmail.com>
2026-05-05 16:27:11 -0500

    `Added validate middleware function`

---
+++ backend/middleware/validate.middleware.js
@@ -0,0 +1,8 @@
+export const validate = (schema) => (req, res, next) => {
+  const { error } = schema.validate(req.body, { abortEarly: false });
+  if (error) {
+    const errors = error.details.map((detail) => detail.message);
+    return res.status(400).json({ errors });
+  }
+  next();
+};
