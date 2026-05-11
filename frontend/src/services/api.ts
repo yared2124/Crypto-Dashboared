3fa2897e9e87df15b1f6eb281124d6794eab3463
yared aregayehu <yaredbaby21@gmail.com>
2026-05-11 17:25:19 -0500

    `Update api.ts to add type annotation to config in request interceptor`

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -14,7 +14,7 @@ const api: AxiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
 });
 
-api.interceptors.request.use((config) => {
+api.interceptors.request.use((config: any) => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
