5fdca8356a3c3a69abac815bfb9bb4ea2580cf4a
yared aregayehu <yaredbaby21@gmail.com>
2026-05-14 17:11:37 -0500

    `Update tailwind.config.js to use color palette from tailwindcss/colors`

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,23 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        cyan: "#00d4ff",
-        emerald: "#10b981",
-        rose: "#f43f5e",
+        cyan: {
+          ...colors.cyan,
+          DEFAULT: "#00d4ff",
+        },
+        emerald: {
+          ...colors.emerald,
+          DEFAULT: "#10b981",
+        },
+        rose: {
+          ...colors.rose,
+          DEFAULT: "#f43f5e",
+        },
       },
       fontFamily: {
         mono: ["IBM Plex Mono", "monospace"],
